86d4761410b81556ac1a5e4121a087ded74b60ec
Ricardo Casares <ricardo.casares@tesco.com>
2022-03-12 14:47:49 +0100

    feat: ui lib

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,10 +1,14 @@
+import "../css/global.css";
 import type { AppProps } from "next/app";
 import { IoProvider } from "socket.io-react-hook";
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <IoProvider>
-      <Component {...pageProps} />
-    </IoProvider>
+    <NextUIProvider>
+      <IoProvider>
+        <Component {...pageProps} />
+      </IoProvider>
+    </NextUIProvider>
   );
 }
--- apps/www/pages/room/[id].tsx
+++ apps/www/pages/room/[id].tsx
@@ -1,39 +1,47 @@
 import { useRouter } from "next/router";
 import { useSocket, useSocketEvent } from "socket.io-react-hook";
-import { FormEventHandler, MouseEventHandler, useState } from "react";
+import { FormEventHandler, useState } from "react";
+import { Avatar, Input, Button, Container, Grid } from '@nextui-org/react';
 import type { User } from "@app/domain";
 
 export default function Home() {
   const [name, setName] = useState("");
+  const [submit, setSubmit] = useState(false);
   const { query: { id } } = useRouter();
   const { socket } = useSocket(process.env.NEXT_PUBLIC_IO_SERVER);
   const { lastMessage: data } = useSocketEvent(socket, 'data');
   const { lastMessage: presence = [] } = useSocketEvent<User[]>(socket, 'presence');
 
+  const disabled = name.length < 3;
+
   const onSubmit: FormEventHandler = (e) => {
     e.preventDefault();
-    console.log(name, id);
+    if (disabled) return;
+    setSubmit(true);
     socket.emit('join', id, name);
   };
 
-  const onClick: MouseEventHandler = (e) => {
-    e.preventDefault();
-    socket.emit('data', { some: 'data' });
-  };
-
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <h1>
-        <form onSubmit={onSubmit}>
-          <input placeholder="your name" type="text" defaultValue={name} onChange={e => setName(e.target.value)} />
-          <button type="submit">Submit</button>
-        </form>
-        <button onClick={onClick}>Data</button>
-        <p>
-          {JSON.stringify(data)}
-        </p>
-        {presence.map((user, id) => <p key={`user-${id}`} style={{ background: user.color }}>{user.name}</p>)}
-      </h1>
-    </div>
+    <Container display="flex" css={{ height: "100%", flexDirection: "column" }}>
+      <div style={{ flex: 1, border: "1px solid red" }}>Conasdadtent</div>
+      <form id="name" style={{ display: "none" }} onSubmit={onSubmit}></form>
+      <Grid.Container css={{ border: "1px solid red" }}>
+        {!submit && <Grid.Container gap={2} css={{ margin: 0 }}>
+          <Grid xs={12}>
+            <Input size="md" width="100%" form="name" label="Name" labelPlaceholder="Name" type="text" defaultValue={name} onChange={e => setName(e.target.value)} />
+          </Grid>
+          <Grid xs={12}>
+            <Button disabled={disabled} flat={!disabled} type="submit" form="name">Start</Button>
+          </Grid>
+        </Grid.Container>}
+        {Boolean(presence.length) && <Grid xs={6}>
+          <Avatar.Group>
+            {presence.map((user, index) => (
+              <Avatar bordered css={{ ".nextui-avatar-bg": { background: user.color }, ".nextui-avatar-text": { color: "white", textTransform: "capitalize" } }} key={index} size="lg" text={user.name} stacked src={`https://avatars.dicebear.com/api/pixel-art-neutral/${user.name}.svg`} />
+            ))}
+          </Avatar.Group>
+        </Grid>}
+      </Grid.Container>
+    </Container >
   );
 }
